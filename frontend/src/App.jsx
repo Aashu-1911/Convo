ac43ef312fecd796420a5fa065151fe82e0b29b1
Aashu-1911 <ashishbiradar.1911@gmail.com>
2025-12-31 21:45:45 +0530

    feat: add routing and new pages for user navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx' 
+import SignupPage from './pages/SignupPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import NotificationsPage from './pages/NotificationsPage.jsx'
+import OnboardingPage from './pages/OnboardingPage.jsx'
+import ChatPage from './pages/ChatPage.jsx'
+import CallPage from './pages/CallPage.jsx'
+import toast, { Toaster } from 'react-hot-toast'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className=' h-screen' data-theme="forest">
+      <button onClick={() => toast.success('This is a toast!')}>Create a toast</button>
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="signup" element={<SignupPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="notifications" element={<NotificationsPage />} />
+        <Route path="onboarding" element={<OnboardingPage />} />
+        <Route path="chat" element={<ChatPage />} />
+        <Route path="call" element={<CallPage />} />
+      </Routes>
+
+      <Toaster />
     </div>
   )
 }
